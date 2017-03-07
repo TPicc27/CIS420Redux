1b0fd2a6da4469c30659eb3b1a394b72e58cebba
koblac01 <almostalice29@gmail.com>
2017-03-06 19:37:10 -0500

    added some alert functionality to advisor dashboard

--- CIS420Redux/Models/Advisor.cs
+++ CIS420Redux/Models/Advisor.cs
@@ -8,5 +8,17 @@ namespace CIS420Redux.Models
     public class Advisor
     {
         public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Title { get; set; }
+        public string Room { get; set; }
+        public string Availability { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
     }
 }
